0fceb2e0b8fb894739dbdb6e5189d85f3d7bd982
Marcin S <marcin@bytedude.com>
2022-05-10 15:08:59 -0500

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,18 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Skynet Token Wallet Foundation`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
+
+export default config;
